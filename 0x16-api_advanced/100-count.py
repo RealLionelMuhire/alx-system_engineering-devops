b005fc152ed5e80be2a433901307dfa6064850f3
RealLionelMuhire <muhirelionel@gmail.com>
2023-11-09 00:16:45 +0200

    pycode fix

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,15 +5,16 @@
 
 import requests
 
+
 def count_words(subreddit, word_list, after=None, counts=None):
     if counts is None:
         counts = {}
 
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    
+
     if after:
         url += f'&after={after}'
-    
+
     headers = {'User-Agent': 'YourAppName/1.0'}
 
     try:
@@ -25,7 +26,7 @@ def count_words(subreddit, word_list, after=None, counts=None):
                 posts = data['data']['children']
                 if not posts:
                     return print_counts(counts)
-                
+
                 for post in posts:
                     title = post['data']['title'].lower()
                     for word in word_list:
@@ -33,7 +34,7 @@ def count_words(subreddit, word_list, after=None, counts=None):
                         if word not in counts:
                             counts[word] = 0
                         counts[word] += title.count(f' {word} ')
-                
+
                 next_page = data['data']['after']
                 return count_words(subreddit, word_list, next_page, counts)
             else:
@@ -45,19 +46,23 @@ def count_words(subreddit, word_list, after=None, counts=None):
     except requests.exceptions.RequestException:
         return print_counts(counts)
 
+
 def print_counts(counts):
-    # Sort counts by value (count) in descending order, and then alphabetically
-    sorted_counts = sorted(counts.items(), key=lambda item: (-item[1], item[0]))
-    
+    # Sort counts by value (count) in descending order, then alphabetically
+    sorted_counts = sorted(counts.items(), key=lambda item: (-item[1],
+                                                             item[0]))
+
     for word, count in sorted_counts:
         if count > 0:
             print(f"{word}: {count}")
+
 
 if __name__ == '__main__':
     import sys
     if len(sys.argv) < 3:
         print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
-        print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
+        print("Ex: {} programming 'python java javascript'"
+              .format(sys.argv[0]))
     else:
         subreddit = sys.argv[1]
         keywords = [x for x in sys.argv[2].split()]
